8c4c67fb8a6302aab9a67549b2edffac9a9e6841
Miumen <sharifigiv5954@gmail.com>
2022-12-19 18:57:18 +0330

    add button 0.1

--- CGCore.py
+++ CGCore.py
@@ -7,12 +7,13 @@ class CubicGUI:
         sdl2.SDL_Init(sdl2.SDL_INIT_VIDEO)
 
         self.win = None
+        self.s = sdl2.SDL_Renderer()
+
+        self.buttons = []
+
         self.running = True
         
     def update(self):
-        if self.win == None:
-            return 0 
-
         # Events  
         event = sdl2.SDL_Event()
 
@@ -25,6 +26,13 @@ class CubicGUI:
 
     def createWin(self, x, y, width, height, title):
         self.win = Window(x, y, width, height, title) 
+
+    def createButton(self, x, y, width, height):
+        b = Button(x, y, width, height, self.s)
+
+        self.buttons.append(b)
+
+        return b
 
 class Window:
     def __init__(self, x, y, width, height, title):
@@ -40,3 +48,24 @@ class Window:
 
     def quit(self):
         sdl2.SDL_DestroyWindow(self.w)
+
+class Button:
+    def __init__(self, x, y, width, height , rn):
+        self.x = x
+        self.y = y 
+
+        self.width = width
+        self.height = height
+
+        self.bg = ()
+
+        self.border = 0 
+        self.borderColor = (0, 0, 0, 0)
+
+        self.radius = 5 
+
+        self.status = True
+
+    def show(self):
+        # SDL 2 code
+        pass
--- test.py
+++ test.py
@@ -3,7 +3,10 @@ from CGCore import *
 app = CubicGUI()
 app.createWin(100, 100, 500, 500, 'TEST')
 
+B = app.createButton(100, 300, 50, 100)
+
 while app.running:
     app.update()
+    B.show()
 
 print('!')
