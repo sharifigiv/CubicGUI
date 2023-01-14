cbe848a0e25e0e92ba6aa4d1ae4faa23c6c9262f
sharifigiv <76777409+sharifigiv@users.noreply.github.com>
2023-01-14 12:36:35 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 from CGCore import * 
 
 app = CubicGUI()
-app.createWin(100, 100, 500, 500, 'TEST')
+app.createWin(100, 100, 1080, 720, 'TEST')
 
-B = app.createButton(100, 300, 50, 100)
+B = app.createButton(10, 10, 150, 500, 'ali')
 
 while app.running:
     app.update()
     B.show()
+
+    B.fontSize = 24
 
 print('!')
