eb8db8a261f17247e353974d4794c26604610bff
Miumen <sharifigiv5954@gmail.com>
2023-03-03 11:47:15 +0330

    Add Shape

---
+++ widgets/CGShape.py
@@ -0,0 +1,13 @@
+import sdl2
+
+class Rectangle:
+    def __init__(self, x, y, width, height, color, rn):
+        pass 
+
+class Circle:
+    def __init__(self, x, y, radius, rn):
+        pass 
+
+class Line:
+    def __init__(self, x1, y1, x2, y2, rn):
+        pass
