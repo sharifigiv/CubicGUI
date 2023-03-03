c5c272b353469c5bfdfbb871137d66a9b58f3eae
Miumen <sharifigiv5954@gmail.com>
2023-03-03 12:09:29 +0330

    Line & Fill example

--- examples/shape.py
+++ examples/shape.py
@@ -4,7 +4,9 @@ from CGCore import *
 app = CubicGUI()
 app.createWin(100, 100, 1080, 720, 'Shape Test')
 
-R1 = app.drawRectangle(100, 100, 50, 20, [255, 255, 255, 255])
+R1 = app.drawRectangle(100, 100, 200, 100, [255, 255, 255, 255], True)
+R2 = app.drawRectangle(500, 500, 300, 150, [255, 0, 0, 255], False)
+L1 = app.drawLine(0, 720 // 2, 1080, 720 // 2, [0, 255, 0, 255])
 
 while app.running:
     app.update()
