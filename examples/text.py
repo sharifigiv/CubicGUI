23d93aedd64633f7e9b9738ad8124079ae98f93e
Miumen <sharifigiv5954@gmail.com>
2023-03-03 13:00:37 +0330

    New example

--- examples/text.py
+++ examples/text.py
@@ -5,6 +5,8 @@ app = CubicGUI()
 app.createWin(100, 100, 1080, 720, 'Text Test')
 
 T1 = app.createText(100, 100, "Mayam Bash", [0, 0, 255, 255], 32)
+T2 = app.createText(200, 300, "Ali Bash!", [255, 255, 255, 255], 54)
+T3 = app.createText(1080 // 2, 720 // 2, "Miumen", [0, 255, 0, 255], 16)
 
 while app.running:
     app.update()
