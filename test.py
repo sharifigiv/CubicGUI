bec87dc95e0c74a0a64198ef17a94d47d8e58325
Miumen <sharifigiv5954@gmail.com>
2023-03-03 09:30:44 +0330

    Add Image test

--- test.py
+++ test.py
@@ -12,6 +12,9 @@ B2 = app.createButton(700, 300, 50, 50, 'akhaz')
 
 T1 = app.createText(100, 100, 'Mayam Bash!', [255, 255, 255, 255], 32)
 
+I1 = app.createImage(100, 100, 477 // 2, 477 // 2, 'assets/images/i.jpeg')
+I2 = app.createImage(100, 400, 477 // 3, 477 // 3, 'assets/images/i.jpeg')
+
 while app.running:
     if B.hovering:
         B.borderWidth = 20
@@ -20,6 +23,9 @@ while app.running:
         B.borderWidth = 0
 
     B2.showing = False
+    B1.showing = False
+    B.showing = False
+
     app.update()
 
 print('!')
