fb384fe53c1fec9619e72084ba350044044b9ce7
Miumen <sharifigiv5954@gmail.com>
2023-03-03 09:31:34 +0330

    Bugfix and ctypes

--- widgets/CGImage.py
+++ widgets/CGImage.py
@@ -1,5 +1,6 @@
 import sdl2 
 import sdl2.sdlimage
+from ctypes import pointer, c_int
 
 class Image:
     def __init__(self, x, y, width, height, file, rn):
@@ -7,7 +8,11 @@ class Image:
         self.y = y
 
         self.img = sdl2.sdlimage.IMG_LoadTexture(rn, str.encode(file))
-        sdl2.SDL_QueryTexture(img, None, None, width, height)
+
+        w = pointer(c_int(width))
+        h = pointer(c_int(height))
+
+        sdl2.SDL_QueryTexture(self.img, None, None, w, h)
 
         self.rect = sdl2.SDL_Rect(x, y, width * 2, height * 2)
         self.rn = rn
