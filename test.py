1f21b69398e5fd1dd30227e2820a2c8334006548
Miumen <sharifigiv5954@gmail.com>
2023-03-03 09:05:25 +0330

    Added Text test

--- test.py
+++ test.py
@@ -10,6 +10,8 @@ B = app.createButton(100, 100, 150, 500, 'ali', testcommand)
 B1 = app.createButton(500, 100, 50, 50, 'gazaz')
 B2 = app.createButton(700, 300, 50, 50, 'akhaz')
 
+T1 = app.createText(100, 100, 'Mayam Bash!', [255, 255, 255, 255], 32)
+
 while app.running:
     if B.hovering:
         B.borderWidth = 20
